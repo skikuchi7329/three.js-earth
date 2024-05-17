2f03bb0eab82dfdb73a94a8bef0423afde86bea3
Shohei Kikuchi <killchance2256@gmail.com>
2024-05-17 19:23:35 +0900

    Create scene, camera, renderer instance

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
-import * as THREE from "./build/three.module.js"
+import * as THREE from "./build/three.module.js";
 
-//シーンの確認
-const scene = new THREE.Scene();
-console.log(scene);
+let scene, camera, renderer;
+
+//シーンを追加
+scene = new THREE.Scene();
+
+//カメラを追加
+camera = new THREE.PerspectiveCamera(
+  50,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+
+//レンダラーを追加
+renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement);
+
+
+
+
+renderer.render(scene, camera);
